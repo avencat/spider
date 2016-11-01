954a30e92583ed518f33f3a9a4bb1f6dac09a2c0
vencat_a <axel.vencatareddy@epitech.eu>
2016-11-01 17:56:09 +0100

    Put Network in Commons files

--- common/inc/Network.hh
+++ common/inc/Network.hh
@@ -1,14 +1,34 @@
 //
-// Network.hh for Spider in /home/touzet_t/epitech/cpp/cpp_spider/common
-// 
+// Core.hh for Spider server in /home/touzet_t/epitech/cpp/cpp_spider/server/src
+//
 // Made by Theo TOUZET
 // Login   <touzet_t@epitech.net>
-// 
-// Started on  Tue Nov  1 13:51:29 2016 Theo TOUZET
-// Last update Tue Nov  1 13:51:50 2016 Theo TOUZET
+//
+// Started on  Tue Nov  1 13:49:07 2016 Theo TOUZET
+// Last update Mar Nov 1 17:42:47 2016
 //
 
 #ifndef NETWORK_HH_
 # define NETWORK_HH_
+
+# include <boost/asio.hpp>
+
+class Network {
+private:
+  boost::asio::ip::tcp::acceptor acceptor;
+  boost::asio::io_service        ioservice;
+  boost::asio::ip::tcp::endpoint endpoint;
+  boost::asio::ip::tcp::socket   socket;
+
+public:
+  Network();
+  virtual ~Network();
+
+  // Methods
+  bool read(std::string &str);
+  bool write(std::string &str);
+  bool accept();
+  bool connect(std::string &ip, std::string &port);
+};
 
 #endif /* !NETWORK_HH_ */
